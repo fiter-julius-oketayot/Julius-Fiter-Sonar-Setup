3389e45283a439d6f5efa19ecaf66d4940c643a5
Julius Oketayot <juliuspetero@outlook.com>
2025-02-28 10:04:06 +0300

    Add sonar analysis V2

--- src/main/java/com/example/juliusfitersonarsetup/JuliusFiterSonarSetupApplication.java
+++ src/main/java/com/example/juliusfitersonarsetup/JuliusFiterSonarSetupApplication.java
@@ -10,4 +10,23 @@ public class JuliusFiterSonarSetupApplication {
         SpringApplication.run(JuliusFiterSonarSetupApplication.class, args);
     }
 
+    public  void runTest(String var1,
+                         String var2,
+                         String var3,
+                         String var4,
+                         String var5,
+                         String var6,
+                         String var7,
+                         String var8,
+                         String var9,
+                         String var10,
+                         String var11,
+                         String var12,
+                         String var13,
+                         String var14,
+                         String var15
+                         ){
+        System.out.println("Hello World");
+    }
+
 }
