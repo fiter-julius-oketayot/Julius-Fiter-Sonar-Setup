77893e597dfef45713ba1b3430d0af0cdb6e2648
Julius Oketayot <juliuspetero@outlook.com>
2025-02-28 09:43:16 +0300

    first commit

---
+++ src/main/java/com/example/juliusfitersonarsetup/JuliusFiterSonarSetupApplication.java
@@ -0,0 +1,13 @@
+package com.example.juliusfitersonarsetup;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JuliusFiterSonarSetupApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JuliusFiterSonarSetupApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/juliusfitersonarsetup/JuliusFiterSonarSetupApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.juliusfitersonarsetup;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JuliusFiterSonarSetupApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
